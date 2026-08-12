1531874c59fd3aedb7ba2b539558dad5919d1d3b
sophi <sophiapeluyera@gmail.com>
2026-08-11 23:25:53 -0400

    Add ShowInput Options

--- src/TheContactBook/ContactBook.cs
+++ src/TheContactBook/ContactBook.cs
@@ -111,6 +111,15 @@ public class ContactBook
 
     private void ShowInputOptions()
     {
+        string inputOptions = "" 
+        +$"{NEXT_PAGE} - Next Page, | [{CREATE_CONTACT}] Create Contact | [{DELETE_CONTACT}] Delete Contact,  | [{DEDUPLICATE_CONTACTS}] Deduplicate Contacts\n"
+        +$"{GOTO_PAGE} - Go to Page, | [{UPDATE_CONTACT}] Update Contact | [{ORDER_CONTACTS}] Order Contacts | [{PAGE_SIZE            }] Change Page Size\n" 
+        +$"{PREV_PAGE} - Previous Page, | [{REVIEW_CONTACT}] Review Contact | [{FIND_CONTACTS}] Find Contacts | [{EXIT                }] Exit\n"
+        +$"\n> ";
+
+
+        Console.WriteLine();
+        Console.Write(inputOptions);
         
     }
 
