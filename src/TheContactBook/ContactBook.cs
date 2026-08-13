4f22d488213671692d84490005de21a45c706487
sophi <sophiapeluyera@gmail.com>
2026-08-12 22:27:23 -0400

    Add Process input

--- src/TheContactBook/ContactBook.cs
+++ src/TheContactBook/ContactBook.cs
@@ -146,9 +146,97 @@ public class ContactBook
 
     private void ProcessInput(string input)
     {
-        
+        switch(input)
+        {
+            case NEXT_PAGE:NextPage();
+                break;
+            case PREV_PAGE:PrevPage();
+                break;
+            case GOTO_PAGE:GotoPage();
+                break; 
+            case PAGE_SIZE:PageSize();
+                break;
+            case CREATE_CONTACT:CreateContact();
+                break;
+            case REVIEW_CONTACT:ReviewContact();
+                break;
+            case UPDATE_CONTACT:UpdateContact();
+                break;
+            case DELETE_CONTACT:DeleteContact();
+                break;  
+            case FIND_CONTACTS:FindContacts();
+                break;
+            case ORDER_CONTACTS:OrderContacts();
+                break;
+            case DEDUPLICATE_CONTACTS:DeduplicateContacts();
+                break;
+            case EXIT:Exit();
+                break;
+            default: break;
+        }
     }
-     private bool ConfirmExit()
+
+    private void NextPage()
+    {
+        Console.WriteLine("Next Page");
+    }
+
+    private void PrevPage()
+    {
+        Console.WriteLine("Previous Page");
+    }
+
+    private void GotoPage()
+    {
+        Console.WriteLine("Go to Page");
+    }
+
+    private void PageSize()
+    {
+        Console.WriteLine("Change Page Size");
+    }
+
+    private void CreateContact()
+    {
+        Console.WriteLine("Create Contact");
+    }
+
+    private void ReviewContact()
+    {
+        Console.WriteLine("Review Contact");
+    }
+
+    private void UpdateContact()
+    {
+        Console.WriteLine("Update Contact");
+    }
+
+    private void DeleteContact()
+    {
+        Console.WriteLine("Delete Contact");
+    }
+
+    private void FindContacts()
+    {
+        Console.WriteLine("Find Contacts");
+    }
+
+    private void OrderContacts()
+    {
+        Console.WriteLine("Order Contacts");
+    }
+
+    private void DeduplicateContacts()
+    {
+        Console.WriteLine("Deduplicate Contacts");
+    }
+
+    private void Exit()
+    {
+        Console.WriteLine("Exit");
+    }
+
+    private bool ConfirmExit()
     {
         return true;
     }
