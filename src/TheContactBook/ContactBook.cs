8173eef02fbed4d3832296162a97f44ad0fcd7cc
sophi <sophiapeluyera@gmail.com>
2026-08-13 20:26:03 -0400

    Add Go to page

--- src/TheContactBook/ContactBook.cs
+++ src/TheContactBook/ContactBook.cs
@@ -220,7 +220,13 @@ public class ContactBook
 
     private void GotoPage()
     {
-        Console.WriteLine("Go to Page");
+        GotoPage(allContacts, ref page, size);
+        
+    }
+
+    private void GotoPage(List<Contact> contacts, ref int page, int size)
+    {
+        page = GetInt("Enter page number: ", 1, PageCount(contacts, size));
     }
 
     private void PageSize()
@@ -267,7 +273,25 @@ public class ContactBook
     {
         isExit = true;
     }
+    
+    private int GetInt(string prompt, int min, int max)
+    {
+        string options = $"{min}-{max}";
 
+        Console.Write(prompt + $"[{options}] ");
+        string answer = Console.ReadLine()!.ToUpper();
+        int value;
+
+        
+        while(!int.TryParse(answer, out value) || value < min || value > max)
+        {
+            Console.WriteLine("Error: Invalid Option. Please try again.");
+            Console.Write(prompt + $"[{options}] ");
+            answer  = Console.ReadLine()!;
+
+        }
+        return value;
+    }
     private string GetOption(string prompt, string [] validOptions, string defaultOption)
     {
         string options = string.Join("/", validOptions);
