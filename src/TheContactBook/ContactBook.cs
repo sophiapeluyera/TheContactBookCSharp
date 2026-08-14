7f5701d0b2f90b554302424cca0921ec8c72129a
sophi <sophiapeluyera@gmail.com>
2026-08-13 20:36:15 -0400

    Add PageSize

--- src/TheContactBook/ContactBook.cs
+++ src/TheContactBook/ContactBook.cs
@@ -231,7 +231,14 @@ public class ContactBook
 
     private void PageSize()
     {
-        Console.WriteLine("Change Page Size");
+        PageSize(ref page , ref size);
+    }
+
+    private void PageSize( ref int page, ref int size)
+    {
+        int max = Console.WindowHeight - 10;
+        size = GetInt("Enter page size: ", 1, max);
+        page =1;
     }
 
     private void CreateContact()
